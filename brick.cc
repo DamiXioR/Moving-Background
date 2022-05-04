b1035355524c16c554410e322993643b355da6c0
Damian <damixiorek@gmail.com>
2022-05-04 17:25:59 +0200

    remove redundant headers from brick impl

--- brick.cc
+++ brick.cc
@@ -1,8 +1,5 @@
 #include "brick.h"
 
-#include <cstdint>
-#include <utility>
-
 // public section
 
 Brick::Brick (uint16_t hightPos, uint16_t widhtPos, char pattern) noexcept : hightPos_(hightPos), widhtPos_(widhtPos), pattern_(pattern) {}
